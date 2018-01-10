5cf847ddc6dfbd6cf172d8610fb9bab142f7a7b5
wckdouglas <wckdouglas@gmail.com>
2018-01-10 11:20:18 -0600

    modified install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/wckdouglas/sequencing_tools.git
-pip install ./sequencing_tools/requirements.txt 
-pip install ./sequencing_tools
+cd sequencing_tools
+pip install requirements.txt 
+pip install .
+
+

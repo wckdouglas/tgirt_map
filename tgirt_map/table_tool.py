0971d9f73d9fda92d9dd53a0753a3289f57cc9ae
wckdouglas <wckdouglas@gmail.com>
2018-05-31 16:42:20 -0500

    changed

--- tgirt_map/table_tool.py
+++ tgirt_map/table_tool.py
@@ -3,12 +3,12 @@
 import re
 
 
-ncRNA = ["sense_intronic","3prime_overlapping_ncRNA",'processed_transcript',
+ncRNA = ["sense_intronic","3prime_overlapping_ncRNA",'processed_transcript','TEC',
         'sense_overlapping','Other_lncRNA', 'macro_lncRNA','non_coding','known_ncrna', 'LRG_gene',
         'lincRNA','bidirectional_promoter_lncRNA', 'ribozyme','3prime_overlapping_ncrna']
 smncRNA = ['misc_RNA','snRNA','piRNA','scaRNA','sRNA','scRNA']
 large_rRNA = ['28S_rRNA','18S_rRNA']
-small_rRNA = ['rRNA','5S_rRNA','58S_rRNA','5.8S_rRNA']
+small_rRNA = ['rRNA','5S_rRNA','58S_rRNA','5.8S_rRNA', 'rDNA']
 protein_coding = ['protein_coding','TR','IG']
 def change_gene_type(x):
     type = ''
